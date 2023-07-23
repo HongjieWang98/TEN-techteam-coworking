e7000471bfceb41cc98740aea70b58c36595de39
Winnie Wang <wangyuling3532@gmail.com>
2023-07-23 09:21:33 -0400

    add CreateListing.js to the booklist branch

---
+++ .history/src/App_20230719160725.js
@@ -0,0 +1,27 @@
+
+import './App.css';
+import ApplicationForm from './CreateListing';
+
+function App() {
+  return (
+    <div className="App">
+     TEN tech-team co-work place
+     Enjoy coding here!
+     <ApplicationForm />
+    </div>
+   
+  );
+}
+
+export default App;
+
+function App() {
+  return (
+    <div className="App">
+     TEN tech-team co-work place
+     Enjoy coding here!
+    </div>
+  );
+}
+
+export default App;
