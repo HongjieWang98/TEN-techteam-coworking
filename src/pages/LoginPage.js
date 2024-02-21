e4dc83022d985e4c6ca7080b7c8fb863737d9265
Hongjie Wang <49068275+HongjieWang98@users.noreply.github.com>
2024-02-21 15:06:07 -0500

    Add authentication sign up in Login Page (#4)

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -2,11 +2,15 @@
 import React from 'react';
 // import { Link }  from 'react-router-dom';
 import { BrowserRouter as Router, Link } from 'react-router-dom';
+// Signup.js
+import Signup from '../components/Signup';;
 
 const LoginPage = (props) => {
   return (
     <>
-        <h1>Login Page</h1>
+        <h1>LoginPage</h1>
+        <Signup/>
+        
         <Link to='/applicationform'>Click Here to go to CreateListing Component</Link>
 
     </>
