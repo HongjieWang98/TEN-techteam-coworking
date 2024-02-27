2a545302068dfbc6f80f0c0f020cef9f24d1d163
Hongjie Wang <1123147983@qq.com>
2024-02-27 18:56:31 -0500

    connectted to Firebase authentication

--- src/firebase/firebase_config.js
+++ src/firebase/firebase_config.js
@@ -4,12 +4,29 @@ import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
 import { getDatabase } from "firebase/database";
 import firebase from 'firebase/compat/app'; 
 import "firebase/compat/firestore";
+import "firebase/compat/auth";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyD71PE__dd3iSsfxnozgj6-KD1cmAb2dTY",
+//   authDomain: "fir-testjuly8.firebaseapp.com",
+//   projectId: "fir-testjuly8",
+//   storageBucket: "fir-testjuly8.appspot.com",
+//   messagingSenderId: "479502811195",
+//   appId: "1:479502811195:web:8059bfd20ad184354c9c1e",
+//   measurementId: "G-3HZ6C1E3QZ"
+// };
+
+// // Initialize Firebase
+
+// const app = firebase.initializeApp(firebaseConfig);
+// const db = getFirestore(app);
+
+// export const auth = app.auth()
+// export {app, db}
+
+
+
 const firebaseConfig = {
   apiKey: "AIzaSyD71PE__dd3iSsfxnozgj6-KD1cmAb2dTY",
   authDomain: "fir-testjuly8.firebaseapp.com",
@@ -25,4 +42,5 @@ const firebaseConfig = {
 const app = firebase.initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
+export const auth = app.auth()
 export {app, db}
