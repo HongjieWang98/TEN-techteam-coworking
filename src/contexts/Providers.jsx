f9fea50de6d0a33b718849b37ae65ef9515a82e5
Kevin Bae <kevinbae15@gmail.com>
2024-03-25 17:16:34 -0400

    [chore] npm scripts for linting (#20)
    * move linters to become scripts
    
    * prettier for files that werent prettified

--- src/contexts/Providers.jsx
+++ src/contexts/Providers.jsx
@@ -1,13 +1,11 @@
-import { SellProvider } from "./SellContext";
-import { AuthProvider } from "./AuthContext";
+import { SellProvider } from './SellContext';
+import { AuthProvider } from './AuthContext';
 
 // TODO move the sell provider to just the /sell routes
 export function Providers({ children }) {
-    return (
-        <AuthProvider>
-            <SellProvider>
-                {children}
-            </SellProvider>
-        </AuthProvider>
-    );
+  return (
+    <AuthProvider>
+      <SellProvider>{children}</SellProvider>
+    </AuthProvider>
+  );
 }
