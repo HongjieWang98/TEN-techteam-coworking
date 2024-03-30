4f5d535ed470b24aac8dda61430ec311f991294a
Kevin Bae <kevinbae15@gmail.com>
2024-03-30 14:26:57 -0400

    create page styling wrapper

--- src/App.jsx
+++ src/App.jsx
@@ -7,23 +7,26 @@ import ConfirmationPage from './pages/Sell/ConfirmationPage';
 import ViewListingPage from './pages/Listing/ViewListingPage';
 import { Providers } from './contexts/Providers';
 import CreateAccount from './pages/CreateAccount';
+import PageWrapper from './pages/PageWrapper';
 
 function App() {
   return (
     <Router>
       <Providers>
-        <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/applicationform" element={<ApplicationForm />} />
-          <Route path="sell">
-            <Route path="list" element={<ListingPage />} />
-            <Route path="confirmation" element={<ConfirmationPage />} />
-          </Route>
-          <Route path="/listing/">
-            <Route path=":listingId" element={<ViewListingPage />} />
-          </Route>
-          <Route path="/signup" element={<CreateAccount />} />
-        </Routes>
+        <PageWrapper>
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/applicationform" element={<ApplicationForm />} />
+            <Route path="sell">
+              <Route path="list" element={<ListingPage />} />
+              <Route path="confirmation" element={<ConfirmationPage />} />
+            </Route>
+            <Route path="/listing/">
+              <Route path=":listingId" element={<ViewListingPage />} />
+            </Route>
+            <Route path="/signup" element={<CreateAccount />} />
+          </Routes>
+        </PageWrapper>
       </Providers>
     </Router>
   );
---
+++ src/pages/PageWrapper.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const pageWrapperStyles = {
+    padding: '50px',
+    margin: '0px',
+    display: 'flex',
+    justifyContent: 'center',
+};
+
+const innerPageWrapperStyles = {
+    maxWidth: '1500px'
+}
+
+
+// Create your Wrapper component
+const PageWrapper = ({ children }) => {
+    return (
+        <div style={pageWrapperStyles}>
+            <div style={innerPageWrapperStyles}>
+                {children}
+            </div>
+        </div>
+    );
+};
+
+export default PageWrapper;
