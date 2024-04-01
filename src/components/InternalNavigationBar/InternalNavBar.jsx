2ce57063b56c924d4e0925afe0eae68f062bc76b
sburchfield33 <stephen_burchfield@comcast.net>
2024-04-01 17:58:40 -0400

    created InternalNavBar

---
+++ src/components/InternalNavigationBar/InternalNavBar.jsx
@@ -0,0 +1,51 @@
+import { Link } from 'react-router-dom';
+import './NavigationBar.css';
+import logo from '../../images/logo2.png';
+import NavigationBarItem from '../ExternalNavigationBar/NavigationBarDef';
+
+function NavigationBar() {
+    // **** @todo Fix the linking of the nav bar items ***
+  return (
+    <div className="NavigationBarContainer">
+      <div
+        className="NavigationBarItemContainer"
+        style={{
+          float: 'left',
+          padding: '5px 0'
+        }}>
+        <Link to="/" className="NavigationBarItemAnchor">
+          <img src={logo} className="NavigationBarImage" alt="TEN Logo" />
+        </Link>
+      </div>
+      <NavigationBarItem
+        float="Left"
+        text="My account"
+        link="/profile"
+        /* id='nav-your-listings' */
+      />
+      <NavigationBarItem
+        float="Left"
+        text="Sell"
+        link="/sell"
+        /* class='NoDisplayAtPhone600'
+        id='nav-about' */
+      />
+      <NavigationBarItem
+        float="Left"
+        text="Buy"
+        link="/buy"
+        /* class='NoDisplayAtPhone480'
+        id='nav-members' */
+      />
+      <NavigationBarItem
+        float="Left"
+        text="Home"
+        link="/home"
+        /* class='NoDisplayAtPhone480'
+        id='nav-members' */
+      />
+    </div>
+  );
+}
+
+export default NavigationBar;
