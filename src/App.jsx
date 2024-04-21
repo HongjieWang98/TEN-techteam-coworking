e99b6670c1a91d3c46062cc378d3c3bca361d81a
sburchfield33 <stephen_burchfield@comcast.net>
2024-04-21 17:40:48 -0400

    Fixed error in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -19,15 +19,14 @@ import { BuyProvider } from './contexts/BuyContext';
 function App() {
   return (
     <Router>
-    
       <ExternalNavigationBar />
       <Routes>
-        <Route path="/" element={<HomePage />}/>
-        <Route path="/Home" element={<HomePage />}/>
-        <Route path="/Home/howto" element={<HowToPage />}/>
-        <Route path="/Home/schoolnav" element={<SchoolNavPage />}/>
-        <Route path="/Home/signin" element={<LoginPage />}/>
-        <Route path="/Home/signup" element={<CreateAccount />}/>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/Home" element={<HomePage />} />
+        <Route path="/Home/howto" element={<HowToPage />} />
+        <Route path="/Home/schoolnav" element={<SchoolNavPage />} />
+        <Route path="/Home/signin" element={<LoginPage />} />
+        <Route path="/Home/signup" element={<CreateAccount />} />
       </Routes>
 
       <Providers>
@@ -35,7 +34,6 @@ function App() {
           <BuyProvider>
             <Routes>
               <Route path="/" element={<LoginPage />} />
-              <Route path="/applicationform" element={<ApplicationForm />} />
               <Route path="sell">
                 <Route path="list" element={<ListingPage />} />
                 <Route path="confirmation" element={<ConfirmationPage />} />
