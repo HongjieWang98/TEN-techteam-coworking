b182ac7ac1e3624957457f8ac4520c0d72945790
gracestapkowski <gracestapkowski@gmail.com>
2024-04-21 17:21:19 -0400

    ListingPage update #2

--- src/pages/Sell/ListingPage.jsx
+++ src/pages/Sell/ListingPage.jsx
@@ -2,16 +2,52 @@ import { useNavigate } from 'react-router-dom';
 import { useRef, useState } from 'react';
 import Input from '../../components/common/Input';
 import { useSellContext } from '../../contexts/SellContext';
-import { set } from "firebase/database";
-import { ref as sRef } from 'firebase/storage';
-import { db } from '../../firebase/firebase_config';
+import { doc, addDoc, collection } from "firebase/firestore/lite"; 
+import { db, app, auth } from '../../firebase/firebase_config';
+import React, { useEffect } from 'react';
 
 
 export default function ListingPage() {
+  const [answer, setAnswer] = useState('');
   const [isLoading, setIsLoading] = useState(false);
   const { setListing } = useSellContext();
   const formRef = useRef(null);
   const navigate = useNavigate();
+  const [currentUser, setCurrentUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribeAuth = app.auth().onAuthStateChanged((user) => {
+      setCurrentUser(user);
+    });
+
+    return () => {
+      unsubscribeAuth();
+    };
+  }, []);
+
+  const userId = currentUser ? currentUser.uid : null;
+
+  const fetchUserData = async () => {
+    if (userId) {
+      try {
+        const userDoc = await app.firestore().collection('users').doc(userId).get();
+        if (userDoc.exists) {
+          const userData = userDoc.data();
+          console.log('User data:', userData);
+        } else {
+          console.log('User document not found');
+        }
+      } catch (error) {
+        console.error('Error fetching user data:', error);
+      }
+    }
+  };
+
+  useEffect(() => {
+    fetchUserData();
+  }, [userId]);
+
+  //handle form submission to upload to database
   async function handleSubmit(e) {
     e.preventDefault();
     setIsLoading(true);
@@ -28,13 +64,16 @@ export default function ListingPage() {
 
       try {
         // const auth = getAuth();
-        set(sRef(db, 'items'), {
+        addDoc(collection(db, 'items'), {
           isbn: data['isbn'],
           title: data['title'],
           author : data['author'],
+          edition: data['edition'],
           course_number: data['courseNumber'],
           price: data['price'],
-          notes: ['notes']
+          notes: data['notes'],
+          condition: answer,
+          seller_id: userId
         });
         navigate('/sell/confirmation/');
       }
@@ -46,9 +85,21 @@ export default function ListingPage() {
     }
   }
 
+  const handleChange = (event) => {
+    setAnswer(event.target.value);
+  }
+
   return (
     <form onSubmit={handleSubmit} ref={formRef}>
       {/* TODO: Need to add input validation */}
+      <Input
+        name="Organization"
+        label="Organization"
+        placeholder="Tufts University"
+        type="text"
+        isLoading={isLoading}
+        required
+      />
       <Input
         name="isbn"
         label="ISBN"
@@ -65,7 +116,12 @@ export default function ListingPage() {
         isLoading={isLoading}
         required
       />
-      <Input name="edition" label="Edition" placeholder="1" type="text" isLoading={isLoading} />
+      <Input name="edition"
+      label="Edition"
+      placeholder="1"
+      type="text"
+      isLoading={isLoading} 
+      />
       <Input
         name="author"
         label="Author"
@@ -97,6 +153,16 @@ export default function ListingPage() {
         isLoading={isLoading}
         required
       />
+      <label>
+          Condition
+          <select value={answer} onChange={handleChange}>
+            <option value="poor">Poor</option>
+            <option value="fair">Fair</option>
+            <option value="good">Good</option>
+            <option value="very good">Very Good</option>
+            <option value="like_new">Like New</option>
+          </select>
+        </label>
       <Input
         name="notes"
         label="Notes (e.g., is an access code or CD included?)"
