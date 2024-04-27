e1ed4903d5c8798dff5b1a4ec7f9e5e9f2741cf1
Kevin Bae <kevinbae15@gmail.com>
2024-04-27 18:26:21 -0400

    [feature] init firebase functions + implement addNewOrg function (#39)
    * init firebase functions + implement addNewOrg function
    
    * add dryRuns
    
    * add created_at fields
    
    * Update README.md
    
    * Update README.md

---
+++ functions/.eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es2021": true,
+    "jest": true
+  },
+  "extends": [
+    "airbnb",
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended",
+    "prettier"
+  ],
+  "parserOptions": {
+    "ecmaVersion": "latest",
+    "sourceType": "module"
+  },
+  "rules": {
+    "react/react-in-jsx-scope": "off",
+    "react/require-default-props": "off",
+    "react/prop-types": "off",
+    "import/prefer-default-export": "off",
+    "linebreak-style": 0
+  }
+};
