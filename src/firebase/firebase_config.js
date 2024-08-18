04a59f767c7fec21570fb8b065b5f14f5c2dbe6a
Kevin Bae <kevinbae15@gmail.com>
2024-08-18 18:27:57 -0400

    Separate out env (#59)
    * setup dev deploys + have creds in env vars
    
    * setup deploys for each env
    
    * small fix

--- src/firebase/firebase_config.js
+++ src/firebase/firebase_config.js
@@ -1,22 +1,19 @@
-// Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore/lite';
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
 import 'firebase/compat/auth';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyD71PE__dd3iSsfxnozgj6-KD1cmAb2dTY',
-  authDomain: 'fir-testjuly8.firebaseapp.com',
-  projectId: 'fir-testjuly8',
-  storageBucket: 'fir-testjuly8.appspot.com',
-  messagingSenderId: '479502811195',
-  appId: '1:479502811195:web:8059bfd20ad184354c9c1e',
-  measurementId: 'G-3HZ6C1E3QZ'
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
-
 const app = firebase.initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = app.auth();
